dc6b264c891d8a6f75b9de7086039b3809d5910f
Efstathios Chatzikyriakidis <contact@efxa.org>
2017-05-29 02:19:59 +0300

    Updates.

--- start-stack.sh
+++ start-stack.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-chown -R $USER $HOME/.composer
+sudo chown -R $USER $HOME/.composer
 
 composer install -d app
 
 ln -sf app/.env .env
 
-find . -type d -exec chmod 755 {} \;
+sudo find -not -path "./mysql/data*" -type d -exec chmod 755 {} \;
 
-find . -type f -exec chmod 644 {} \;
+sudo find -not -path "./mysql/data*" -type f -exec chmod 644 {} \;
 
 sudo chgrp -R www-data app
 
-find app/storage -type d -exec chmod 775 {} \;
+sudo find app/storage -type d -exec chmod 775 {} \;
 
-find app/storage -type f -exec chmod 664 {} \;
+sudo find app/storage -type f -exec chmod 664 {} \;
 
 docker-compose up --force-recreate -d
