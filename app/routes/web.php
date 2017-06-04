28121811392024cd6806ce3e5811923bb76b3808
Efstathios Chatzikyriakidis <contact@efxa.org>
2017-06-04 19:56:27 +0300

    Minor changes.

--- app/routes/web.php
+++ app/routes/web.php
@@ -11,23 +11,23 @@ $app->group(['prefix' => 'api/v1'], function () use ($app)
 {
     // Authentication
 
-    $app->post('authentication','AuthenticationController@login');
+    $app->post('authentication', 'AuthenticationController@login');
 
     // Users
 
-    $app->get('users','UserController@index');
+    $app->get('users', 'UserController@index');
 
-    $app->post('users','UserController@create');
+    $app->post('users', 'UserController@create');
 
     // Sensor Types
 
-    $app->get('sensorTypes','SensorTypeController@index');
+    $app->get('sensorTypes', 'SensorTypeController@index');
 
     // Sensors
 
-    $app->get('sensors','SensorController@index');
+    $app->get('sensors', 'SensorController@index');
 
-    $app->post('sensors','SensorController@create');
+    $app->post('sensors', 'SensorController@create');
 
     $app->put('sensors/{id:\d+}', 'SensorController@update');
 
