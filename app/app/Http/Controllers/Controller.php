c6bac5ca59425d836b66f2033201398627ce0c5a
Efstathios Chatzikyriakidis <contact@efxa.org>
2017-06-04 20:55:04 +0300

    Add base helpers for json and empty responses in controllers.

--- app/app/Http/Controllers/Controller.php
+++ app/app/Http/Controllers/Controller.php
@@ -1,10 +1,20 @@
 <?php
 
+// Change here.
+
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function json_response($data, $status_code) {
+        return response()->json(['data' => $data], $status_code);
+    }
+
+    public function empty_response($status_code) {
+        return response(null, $status_code);
+    }
 }
+
+?>
