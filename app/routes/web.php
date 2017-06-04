d3731b3233414aa116cacea199b1dba63d3d5715
Efstathios Chatzikyriakidis <contact@efxa.org>
2017-06-04 19:54:51 +0300

    Add new routes with users/sensors/sensorTypes and authentication.

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,15 +2,36 @@
 
 // Change here.
 
-$app->get('/', function () use ($app) {
+$app->get('/', function () use ($app)
+{
     return $app->version();
 });
 
-$app->group(['prefix' => 'api/v1'], function($app)
+$app->group(['prefix' => 'api/v1'], function () use ($app)
 {
+    // Authentication
+
+    $app->post('authentication','AuthenticationController@login');
+
+    // Users
+
     $app->get('users','UserController@index');
 
-    $app->post('users','UserController@createUser');
+    $app->post('users','UserController@create');
+
+    // Sensor Types
+
+    $app->get('sensorTypes','SensorTypeController@index');
+
+    // Sensors
+
+    $app->get('sensors','SensorController@index');
+
+    $app->post('sensors','SensorController@create');
+
+    $app->put('sensors/{id:\d+}', 'SensorController@update');
+
+    $app->delete('sensors/{id:\d+}', 'SensorController@delete');
 });
 
 ?>
