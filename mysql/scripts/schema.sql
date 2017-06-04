bf0cb8f32429385a628cae9960a2f08f179c32fa
Efstathios Chatzikyriakidis <contact@efxa.org>
2017-06-04 19:09:45 +0300

    Add new migration script for database.

--- mysql/scripts/schema.sql
+++ mysql/scripts/schema.sql
@@ -37,7 +37,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,8 +46,60 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2017_05_24_171235_create_table_users',1);
+INSERT INTO `migrations` VALUES (1,'2017_05_30_212638_create_table_users',1),(2,'2017_05_30_212653_create_table_sensor_types',1),(3,'2017_05_30_212658_create_table_sensors',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sensor_types`
+--
+
+DROP TABLE IF EXISTS `sensor_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sensor_types` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensor_types`
+--
+
+LOCK TABLES `sensor_types` WRITE;
+/*!40000 ALTER TABLE `sensor_types` DISABLE KEYS */;
+INSERT INTO `sensor_types` VALUES (1,'Soil Moisture'),(2,'Temperature'),(3,'PH');
+/*!40000 ALTER TABLE `sensor_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sensors`
+--
+
+DROP TABLE IF EXISTS `sensors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sensors` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `label` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `point_latitude` decimal(9,7) NOT NULL,
+  `point_longitude` decimal(9,7) NOT NULL,
+  `sensor_type_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sensors_sensor_type_id_foreign` (`sensor_type_id`),
+  CONSTRAINT `sensors_sensor_type_id_foreign` FOREIGN KEY (`sensor_type_id`) REFERENCES `sensor_types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensors`
+--
+
+LOCK TABLES `sensors` WRITE;
+/*!40000 ALTER TABLE `sensors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sensors` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -59,11 +111,14 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `lastname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `api_key` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`),
+  UNIQUE KEY `users_api_key_unique` (`api_key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -73,7 +128,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Efstathios','Chatzikyriakidis','echatzikyriakidis','nopasswords'),(2,'Vagelis','Mitropoulos','vmitropoulos','nopasswords');
+INSERT INTO `users` VALUES (1,'Efstathios','Chatzikyriakidis','contact@efxa.org','$2y$10$tCPvNjIHtG83Zrdieo2tT.vwGlCkaTzoW.qGe8Z/GFZgJ32yELf4G',NULL),(2,'Vagelis','Mitropoulos','vagelis@mitropoulos.com','$2y$10$GdJhnnJY1iET8UbSpy9N9uTec38.axrCctFJj6zqd9crLSURmZYfa',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -86,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-27 17:08:37
+-- Dump completed on 2017-06-04 13:55:30
