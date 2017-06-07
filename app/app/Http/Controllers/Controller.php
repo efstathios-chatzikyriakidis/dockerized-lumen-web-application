db40a535bc7ac0fcd53f240bb911cf3610c231c5
Efstathios Chatzikyriakidis <contact@efxa.org>
2017-06-08 00:24:39 +0300

    Add json reponses.

--- app/app/Http/Controllers/Controller.php
+++ app/app/Http/Controllers/Controller.php
@@ -8,8 +8,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public function json_response($data, $status_code) {
+    public function data_json_response($data, $status_code) {
         return response()->json(['data' => $data], $status_code);
+    }
+
+    public function json_response($data, $status_code) {
+        return response()->json($data, $status_code);
     }
 
     public function empty_response($status_code) {
